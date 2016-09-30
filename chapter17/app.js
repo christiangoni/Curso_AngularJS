b27ead9529f87d4b5e4f29b8a8638412f57642e0
Christian Goñi <chistiansen@gmail.com>
2016-09-30 17:21:15 +0200

    chapter17 ended and working

--- chapter17/app.js
+++ chapter17/app.js
@@ -1,4 +1,17 @@
 angular.module("CustomDirective",[])
+// backImg en la vista sera back-img
+.directive('backImg',function(){
+    // scope, elemento html(div),attributos(clases,etc)
+    return function(scope,element,attrs){
+        attrs.$observe('backImg',function(value){
+            element.css({
+                'background':"url("+value+")",
+                "background-size":"cover",
+                "background-position":"center"
+            })
+        });
+    }
+})
 .controller("AppCtrl",function($scope,$http){
     $http.get("https://api.github.com/users/christiangoni/repos")
     .success(function(data){
